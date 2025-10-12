b2182e539dc719aff2d86e70318b2a29bd0260b7
Devanshugoell <devgoyal952@gmail.com>
2025-10-12 14:02:12 +0530

    Enhance ResCardShimmer component with detailed shimmer structure and styling; update RoundShimmer height for consistency.

--- src/shimmers/ResCardShimmer.jsx
+++ src/shimmers/ResCardShimmer.jsx
@@ -2,21 +2,111 @@ const ResCardShimmer = () => {
   return (
     <>
       <div className="flex flex-wrap justify-center items-center animate-pulse">
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
+        <div className="shimmer-card">
+          <div className="shimmer-img w-[95%] h-1/2 bg-slate-300 mx-auto rounded-lg"></div>
+          <div className="shimmer-heading w-[35%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-para w-[45%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-desc w-[85%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-location w-[40%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+        </div>
+       <div className="shimmer-card">
+          <div className="shimmer-img w-[95%] h-1/2 bg-slate-300 mx-auto rounded-lg"></div>
+          <div className="shimmer-heading w-[35%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-para w-[45%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-desc w-[85%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-location w-[40%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-img w-[95%] h-1/2 bg-slate-300 mx-auto rounded-lg"></div>
+          <div className="shimmer-heading w-[35%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-para w-[45%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-desc w-[85%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-location w-[40%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-img w-[95%] h-1/2 bg-slate-300 mx-auto rounded-lg"></div>
+          <div className="shimmer-heading w-[35%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-para w-[45%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-desc w-[85%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-location w-[40%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-img w-[95%] h-1/2 bg-slate-300 mx-auto rounded-lg"></div>
+          <div className="shimmer-heading w-[35%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-para w-[45%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-desc w-[85%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-location w-[40%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-img w-[95%] h-1/2 bg-slate-300 mx-auto rounded-lg"></div>
+          <div className="shimmer-heading w-[35%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-para w-[45%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-desc w-[85%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-location w-[40%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+        </div>
+<div className="shimmer-card">
+          <div className="shimmer-img w-[95%] h-1/2 bg-slate-300 mx-auto rounded-lg"></div>
+          <div className="shimmer-heading w-[35%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-para w-[45%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-desc w-[85%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-location w-[40%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+        </div>
+       <div className="shimmer-card">
+          <div className="shimmer-img w-[95%] h-1/2 bg-slate-300 mx-auto rounded-lg"></div>
+          <div className="shimmer-heading w-[35%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-para w-[45%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-desc w-[85%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-location w-[40%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-img w-[95%] h-1/2 bg-slate-300 mx-auto rounded-lg"></div>
+          <div className="shimmer-heading w-[35%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-para w-[45%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-desc w-[85%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-location w-[40%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-img w-[95%] h-1/2 bg-slate-300 mx-auto rounded-lg"></div>
+          <div className="shimmer-heading w-[35%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-para w-[45%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-desc w-[85%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-location w-[40%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-img w-[95%] h-1/2 bg-slate-300 mx-auto rounded-lg"></div>
+          <div className="shimmer-heading w-[35%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-para w-[45%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-desc w-[85%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-location w-[40%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-img w-[95%] h-1/2 bg-slate-300 mx-auto rounded-lg"></div>
+          <div className="shimmer-heading w-[35%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-para w-[45%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-desc w-[85%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-location w-[40%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-img w-[95%] h-1/2 bg-slate-300 mx-auto rounded-lg"></div>
+          <div className="shimmer-heading w-[35%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-para w-[45%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-desc w-[85%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-location w-[40%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+        </div>
+       <div className="shimmer-card">
+          <div className="shimmer-img w-[95%] h-1/2 bg-slate-300 mx-auto rounded-lg"></div>
+          <div className="shimmer-heading w-[35%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-para w-[45%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-desc w-[85%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-location w-[40%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+        </div>
+        <div className="shimmer-card">
+          <div className="shimmer-img w-[95%] h-1/2 bg-slate-300 mx-auto rounded-lg"></div>
+          <div className="shimmer-heading w-[35%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-para w-[45%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-desc w-[85%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+          <div className="shimmer-location w-[40%] h-3 bg-slate-300 mt-4  rounded-lg"></div>
+        </div>
       </div>
     </>
   );
